9dd8be5657ad7d3bfdfa5c512bc53f0566d68db2
Jonathan Guerra <jonathanhttp123@gmail.com>
2022-12-02 13:14:40 -0500

    textarea

--- front.py
+++ front.py
@@ -2,6 +2,7 @@
 from flask import Flask
 from flask import render_template
 import os
+from flask import request
 ##llamado a flask
 app = Flask(__name__)
 
@@ -29,14 +30,13 @@ def info():
     jona=os.path.join(app.config['UPLOAD_FOLDER'], 'jona.jpg')
     alexis=os.path.join(app.config['UPLOAD_FOLDER'], 'alexis.jpeg')
     return render_template("info.html", c=cami,g=gus,j=joss,jo=jona,a=alexis)
-    
-    
-    
+       
 
+@app.route('/resultados')
+def resultados():
+    user_input = request.args.get('user_input')
+    return render_template('resultados.html',result=user_input)
 
-@app.route('/carga')
-def carga():
-    return render_template('carga.html')
 
 
 ##ejecutar el servicio web
