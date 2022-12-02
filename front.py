c169e9c1f5a9c759a02b7c68aa802a6dd1bb7cd4
GusAdolf <guta.contre@hotmail.com>
2022-12-02 12:36:09 -0500

    correccion y carga de imagenes con flask

--- front.py
+++ front.py
@@ -1,11 +1,19 @@
 #IMPORTAR LIBRERIA PARA USAR FRAMEWORK FLASK
 from flask import Flask
 from flask import render_template
-
+import os
 ##llamado a flask
 app = Flask(__name__)
 
+IMG_FOLDER = os.path.join('static', 'IMG')
+
+app.config['UPLOAD_FOLDER'] = IMG_FOLDER
+
+
+
 ##servicio web
+#Carga de IMAGENES 
+
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -14,7 +22,16 @@ def home():
 
 @app.route('/info')
 def info():
-    return render_template('info.html')
+    
+    cami = os.path.join(app.config['UPLOAD_FOLDER'], 'cami.jpg')
+    gus=os.path.join(app.config['UPLOAD_FOLDER'], 'gus.jpg')
+    joss=os.path.join(app.config['UPLOAD_FOLDER'], 'joss.jpg')
+    jona=os.path.join(app.config['UPLOAD_FOLDER'], 'jona.jpg')
+    alexis=os.path.join(app.config['UPLOAD_FOLDER'], 'alexis.jpeg')
+    return render_template("info.html", c=cami,g=gus,j=joss,jo=jona,a=alexis)
+    
+    
+    
 
 
 @app.route('/carga')
