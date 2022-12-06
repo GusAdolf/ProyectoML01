6071ea197419a6e7adb81c818990d3c5684c9e2c
alexisvg2520 <78839355+alexisvg2520@users.noreply.github.com>
2022-12-06 12:00:09 -0500

    post frase cambio pront

--- front.py
+++ front.py
@@ -22,6 +22,7 @@ def home():
 def frase():
     return render_template('frase.html')
 
+#Documento
 @app.route('/success', methods = ['POST'])  
 def success():  
     if request.method == 'POST':  
@@ -40,6 +41,7 @@ def success():
         Coseno = backend.cosenoVect(colecGeneral)
         return render_template("carga.html", name = f.filename,matrix1 = Jaccard, coseno = Coseno)
 
+#Colección
 @app.route('/success2', methods = ['POST'])  
 def success2():  
     if request.method == 'POST':  
@@ -69,21 +71,22 @@ def info():
     return render_template("info.html", c=cami,g=gus,j=joss,jo=jona,a=alexis)
        
 
-@app.route('/resultados')
+@app.route('/resultados', methods = ['POST'])
 def resultados():
-    user_input = request.args.get('user_input')
-    racismo = backend.cargaListas('static/FILES/racismo.txt')
-    idenGenero = backend.cargaListas('static\FILES\idenGen.txt')
-    clase = backend.cargaListas('static\FILES\clase.txt')
-    edad = 'asd'
-    d1 = str(user_input)
-    """Construcción de Colección con listas discriminatorias"""
-    colecGeneral = [d1,racismo,idenGenero,clase,edad]
-    """JACCARD"""
-    Jaccard = backend.jaccardCompleto(colecGeneral)
-    """COSENO"""
-    Coseno = backend.cosenoVect(colecGeneral)
-    return render_template('resultados.html',result=user_input,matrix1 = Jaccard, coseno = Coseno)
+    if request.method == 'POST':  
+        user_input = request.args.get('user_input')
+        racismo = backend.cargaListas('static/FILES/racismo.txt')
+        idenGenero = backend.cargaListas('static\FILES\idenGen.txt')
+        clase = backend.cargaListas('static\FILES\clase.txt')
+        edad = 'asd'
+        d1 = str(user_input)
+        """Construcción de Colección con listas discriminatorias"""
+        colecGeneral = [d1,racismo,idenGenero,clase,edad]
+        """JACCARD"""
+        Jaccard = backend.jaccardCompleto(colecGeneral)
+        """COSENO"""
+        Coseno = backend.cosenoVect(colecGeneral)
+        return render_template('resultados.html',result=user_input,matrix1 = Jaccard, coseno = Coseno)
 
 
 
